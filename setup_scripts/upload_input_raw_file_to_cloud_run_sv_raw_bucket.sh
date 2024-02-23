efc72a15d149c21f3e0a3f0d26748b3d41779bac
Mazlum TOSUN <mazlum.tosun@gmail.com>
2024-02-23 17:17:08 +0100

    Correction in the service URLs from the Workflow concerning Cloud Run. Correction also the in setup script that uploads the raw file to the input bucket.

--- setup_scripts/upload_input_raw_file_to_cloud_run_sv_raw_bucket.sh
+++ setup_scripts/upload_input_raw_file_to_cloud_run_sv_raw_bucket.sh
@@ -1,1 +1,1 @@
-gsutil cp setup_scripts/files/world_cup_team_players_stats_raw_ndjson.json gs://event-driven-services-qatar-fifa-world-cup-stats-raw/input/stats/world_cup_team_players_stats_raw_ndjson.json
+gsutil cp setup_scripts/files/world_cup_team_players_stats_raw_ndjson.json gs://event-driven-services-qatar-fifa-world-cup-stats-raw-wf/input/stats/world_cup_team_players_stats_raw_ndjson.json
